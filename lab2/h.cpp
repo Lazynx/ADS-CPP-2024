8b6dc809726fbabeb6b97dd68870a52b30aed047
Lazynx <frichaplay@gmail.com>
2024-09-22 15:14:11 +0500

    2nd lab

--- lab2/h.cpp
+++ lab2/h.cpp
@@ -12,31 +12,143 @@ struct Node{
 };
  
 Node* insert(Node* head, Node* node, int p){
-    /* write your code here */
+    if (p == 0) {
+        node->next = head;
+        head = node;
+        return head;
+    }
+
+    Node* cur = head;
+    for (int i = 0; i < p - 1; i++)
+    {
+        cur = cur->next;
+    }
+    
+    node->next = cur->next;
+    cur->next = node;
+    
+    return head;    
 }
  
 Node* remove(Node* head, int p){
-    /* write your code here */
+    if (p == 0) {
+        head = head->next;
+        return head;
+    }
+
+    Node* cur = head;
+    for (int i = 0; i < p - 1; i++)
+    {
+        cur = cur->next;
+    }
+    
+    Node* temp = cur->next;
+    cur->next = cur->next->next;
+    delete temp;
+    
+    return head;
 }
  
 Node* replace(Node* head, int p1, int p2){
-    /* write your code here */
+    if(p1 == p2) {
+        return head;
+    }
+    Node* cur = head;
+    Node* move = nullptr;
+    Node* prev = nullptr;
+
+    if(p1 == 0) {
+        move = head;
+        head = head->next;
+    } else {
+        for(int i = 0; i < p1; i++) {
+            prev = cur;
+            cur = cur->next;
+        }
+
+        move = cur;
+        prev->next = cur->next; 
+    }
+
+    if(p2 == 0) {
+        move->next = head;
+        head = move;
+    } else {
+        Node* insert = head;
+        for(int i = 0; i < p2 - 1; i++) {
+            insert = insert->next;
+        }
+
+        move->next = insert->next;
+        insert->next = move;
+    }    
+
+    return head;
 }
  
 Node* reverse(Node* head){
-    /* write your code here */
+    if (head == nullptr || head->next == nullptr) return head;
+
+    Node* prev = nullptr;
+    Node* cur = head;
+    Node* next = nullptr;
+
+    while (cur != nullptr) {
+        next = cur->next; 
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+
+    return prev;
 }
  
 void print(Node* head){
-    /* write your code here */
+    Node* cur = head;
+    while (cur!= nullptr) {
+        cout << cur->val << " ";
+        cur = cur->next;
+    }
+    cout << endl;
 }
  
 Node* cyclic_left(Node* head, int x){
-    /* write your code here */
+    Node* cur = head;
+
+    while (cur->next != nullptr) {
+        cur = cur->next;
+    }
+
+    while (x--) {
+        cur->next = head;
+        head = head->next;
+        cur = cur->next;
+        cur->next = nullptr;
+    }
+
+    return head;
 }
  
 Node* cyclic_right(Node* head, int x){
-    /* write your code here */
+    Node* cur = head;
+    int length = 1;
+
+    while (cur->next != nullptr) {
+        cur = cur->next;
+        length++;
+    }
+
+    cur->next = head;
+    x = length - x;
+
+    while (x--) {
+        cur = cur->next;
+    }
+
+    head = cur->next;
+    cur->next = nullptr;    
+
+    return head;
 }
  
 int main(){
